84e32ef03261f5eafdea4a4ebcca0e969f2b0b3e
sergo <sergokov@gmail.com>
2019-05-31 00:28:48 +0300

    Path params to bash script 2.

--- run_crawl_user_followers_bot.sh
+++ run_crawl_user_followers_bot.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python3 run_crawl_user_followers_bot.py $1 $2 $3 /home/sergo/InstaPyWorkspace
+python3 run_crawl_user_followers_bot.py $1 $2 $3 $5
--- run_mass_following_bot.sh
+++ run_mass_following_bot.sh
@@ -2,4 +2,4 @@
 
 sleep .$[ ( $RANDOM % 20 ) + 1 ]m
 
-python3 run_mass_following_bot.py $1 $2 $3 $4 /home/sergo/InstaPyWorkspace
+python3 run_mass_following_bot.py $1 $2 $3 $4 $5
